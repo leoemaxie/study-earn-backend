2efeea98582e5e3a65ece790a7fa184e96bcb009
Emmanuel Lafenwa <103007301+leoemaxie@users.noreply.github.com>
2024-10-11 18:42:20 +0000

    Changed MongoDB connection URL to match env

--- src/chat/index.ts
+++ src/chat/index.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import {Server} from 'socket.io';
 import {connect} from 'mongoose';
 
-const URL = process.env.MONGO_URL || 'mongodb://localhost:27017/chat';
+const URL = process.env.MONGO_URI || 'mongodb://localhost:27017/chat';
 
 (async () => {
   try {
